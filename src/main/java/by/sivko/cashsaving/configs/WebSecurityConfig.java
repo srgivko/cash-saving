083bb8c12de414ab5988f57c650a92a720e8b473
Ivko Serhei <pilsik1991@gmail.com>
2019-09-23 16:11:49 +0300

    add web security config and include in AppInit this config

---
+++ src/main/java/by/sivko/cashsaving/configs/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package by.sivko.cashsaving.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@PropertySource({"classpath:spring-security.properties"})
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.enable-csrf}")
+    private boolean csrfEnabled;
+
+    // TODO: 9/23/19 Autowired userDetailsService for security
+    private UserDetailsService userDetailsService = null;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/user").access("hasRole('USER')")
+                .antMatchers("/admin").access("hasRole('ADMIN')")
+                .antMatchers("/superadmin").access("hasRole('SUPERADMIN')")
+                .and().formLogin().defaultSuccessUrl("/", false);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+   /* @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }*/
+}
