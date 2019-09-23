0f45c16f958b002fc5ee43fc552f2c4b6dc4ef41
Ivko Serhei <pilsik1991@gmail.com>
2019-09-23 18:05:48 +0300

    Add SpringSecurityInitializer to use springSecurityFilterChain

---
+++ src/main/java/by/sivko/cashsaving/configs/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package by.sivko.cashsaving.configs;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
